a5d325329405d8873e9ae97375812da1c47f24b5
Sagar <sagarrsethi10@gmail.com>
2024-11-23 00:47:30 +0530

    added tags and db server

--- v1Main.tf
+++ v1Main.tf
@@ -3,10 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "New_Web_Server" {
+    
   instance_type          = "t2.micro"
   ami                    = "ami-012967cc5a8c9f891"
   key_name               = "SagarKey"
   vpc_security_group_ids = [aws_security_group.MyFirst_SG.id]
+
+  tags = {
+    Name = "Web_Server"
+  }
 }
 
 resource "aws_security_group" "MyFirst_SG" {
@@ -17,4 +22,16 @@ resource "aws_security_group" "MyFirst_SG" {
     protocol  = "tcp"
   }
 
+}
+
+resource "aws_instance" "DB_Server" {
+    instance_type          = "t2.micro"
+  ami                    = "ami-012967cc5a8c9f891"
+  key_name               = "SagarKey"
+  vpc_security_group_ids = [aws_security_group.MyFirst_SG.id]
+
+  tags = {
+    Name = "DB_Server"
+  }
+  
 }
